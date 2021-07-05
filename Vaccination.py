51cd3005ceaa15bc59a8a34eb1aebe2ce804e9ad
DESKTOP-O2UQO9I\Aswath <aswath.chandrasekaran@stud.th-deg.de>
2021-07-05 21:36:16 +0200

    getters and setters for the simple vaccination program

--- Vaccination.py
+++ Vaccination.py
@@ -1,18 +1,43 @@
+# This program displays basic vaccination details of a person which uses only lists,dicts,getters and setters
+
 from Userlogin import userlogin
 class vaccination():
 
     def __init__(self,Name,Vaccination,date):
-        self.Name=Name
-        self.vaccination=Vaccination
-        self.date=date
+        self.__Name=Name
+        self.__vaccination=Vaccination
+        self.__date=date
 
     def info(self):
         userlogin.main()
         userlogin.clear()
-        data=dict(zip(self.vaccination,self.date))
-        return self.Name+" is vaccinated with \n"+ ("\n".join("{}\t{}".format(k,v)for k,v in data.items()))
+        data=dict(zip(self.__vaccination,self.__date))
+        return self.__Name+" is vaccinated with \n"+ ("\n".join("{}\t{}".format(k,v)for k,v in data.items())) "in"
 
-a=vaccination("asw",["Covid","Polio","TB"],["19-05-2020","20.05.1999","19-08-2016"])
+    #getters
+    def getName(self):
+        return self.__Name
+
+    def getvaccination(self):
+        return self.__vaccination
+
+    def getdate(self):
+        return self.__date
+
+    #setters
+    def setName(self,name):
+        self.__Name=name
+
+    def setvaccination(self,vaccination):
+        self.__vaccination=vaccination
+
+    def setdate(self,date):
+        self.__date=date
+
+
+  
+a=vaccination("asw",["Covid","Polio","TB"],["19-05-2020","20-05-1999","19-08-2016"])
+print(a.getName())
+a.setName("Aswath")
+print(a.getName())
 print(a.info())
-
-        
