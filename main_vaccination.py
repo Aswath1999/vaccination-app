7c4bbd53e8fa9ae331994dc19df8fbee54f602e2
DESKTOP-O2UQO9I\Aswath <aswath.chandrasekaran@stud.th-deg.de>
2021-07-06 12:04:17 +0200

    sorted index and need to find a way to bring the pandas dataframe sting to output string

--- main_vaccination.py
+++ main_vaccination.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
 from Userlogin import userlogin
 
-class vaccination: 
+class vaccination(userlogin): 
 
     def __init__(self,Name,Vaccination,dateofvaccination,QRcode):
         self.__Name=Name
@@ -11,13 +12,14 @@ class vaccination:
     
     def writeinfo(self):
         info=pd.DataFrame([[self.__Name,self.__vaccination,self.__vaccinationdate,self.__code]])
-        info=pd.read_csv('Vaccination.csv',names=["Name","Vaccination","Date","QR code"],usecols=["QR code"]).drop_duplicates(keep='first').reset_index()
+        # info=pd.read_csv('Vaccination.csv',names=["Name","Vaccination","Date","QR code"]).drop_duplicates(keep='first').reset_index()
         # info=info.drop_duplicates(subset=None,keep='first',inplace=False,ignore_index=False)
-        info.to_csv("Vaccination.csv",mode="a",index=False,header=False)
+        info.to_csv("Vaccination.csv",mode="a",header=False)
     
     def readinfo(self):
-        info=pd.read_csv('Vaccination.csv',names=["Name","Vaccination","Date","QR code"])
-        print(info[info['Name']==self.__Name])
+        info=pd.read_csv('Vaccination.csv',names=["Name","Vaccination","Date","QR code"]).drop_duplicates(keep='first')
+        return info[info['Name']==self.__Name].reset_index(drop=True)
+        
 
     def updateinfo(self,Oldinfo,newinfo):
         self.__oldinfo=Oldinfo
@@ -31,9 +33,9 @@ class vaccination:
         userlogin.login()
         userlogin.clear()
         self.vaccination=vaccination
-        info=pd.read_csv('Vaccination.csv',names=["Name","Vaccination","Date","QR code"])
-        info=info.drop_duplicates(subset="QR code",keep='last',inplace=False,ignore_index=False)
-        return (info[info["Vaccination"]==vaccination])
+        info=pd.read_csv('Vaccination.csv',names=["Name","Vaccination","Date","QR code"]).drop_duplicates(keep='last')
+        # info=info.drop_duplicates(subset="QR code",keep='last',inplace=False,ignore_index=False)
+        return info[info["Vaccination"]==vaccination].reset_index(drop=True)
 
 
     
@@ -43,10 +45,12 @@ a=vaccination("Gokul","Polio","18.04.2020","A1732105")
 b=vaccination("Aswath","Polio","18.04.2020","A1732115")
 c=vaccination("Dhin","Polio","18.04.2020","A1732101")
 d=vaccination("Dhin","Covid","18.04.2020","A1732401")
-d.writeinfo()
-a.writeinfo()
+e=vaccination("Aswath","Covid","18.04.2020","A4732401")
 b.writeinfo()
+a.writeinfo()
+d.writeinfo()
+e.writeinfo()
 c.writeinfo()
-print(a.readinfo())
+print(b.infoforprofessionals())
 # print(a.updateinfo("Gokul","Asw"))
 # print(a.infoforprofessionals("Covid")
